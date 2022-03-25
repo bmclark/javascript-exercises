9bb96ffcb2ad019c60f2a2bd4a332a5af616fe1c
Bryan Clark <bryan@bclark.net>
2022-03-24 21:40:07 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  //iterate backwards through string into new string
+  let reversedString = '';
+  let i = string.length;
+  
+  while(i >= 0) {
+    reversedString += string.charAt(i);
+    i--;
+  }
+  return reversedString
 };
 
 // Do not edit below this line
