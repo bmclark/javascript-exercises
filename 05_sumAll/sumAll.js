6c3c6a567a71daf6626f4c870e9c0a0021d643a9
Bryan Clark <bryan@bclark.net>
2022-03-25 01:05:49 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...inputs) {
+  let args = Array.from(inputs);
+  args.sort();
+  let a = args[0];
+  let b = args[1];
+  let sum = 0;
 
+  // return errors for negative or non-number inputs
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < 0 ) {
+      return "ERROR";
+    } else if (typeof args[i] != "number") {
+      return "ERROR";
+    }
+  }
+
+  while (a <= b) {
+    sum += a;
+    a++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
