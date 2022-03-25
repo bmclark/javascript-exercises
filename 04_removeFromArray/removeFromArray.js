3cbf31ee37c0a08982ca4f31da38f388af379df0
Bryan Clark <bryan@bclark.net>
2022-03-25 00:44:08 -0400

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(startingArray, ...remove) {
+  let args = Array.from(remove);
 
-};
+  // .filter passes the array element to the arrow function which then checks
+  // the to see if the element (item) is in the args array. If the element is 
+  // found in the args array .includes returns true. The ! operator makes it
+  // false so that the element is NOT included in the new, filtered array.  
+  startingArray = startingArray.filter(item => !args.includes(item));
+
+  return startingArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
