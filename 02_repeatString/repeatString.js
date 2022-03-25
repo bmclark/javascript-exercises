5459fa7d6d40b18a38eaea4025f8e8ee81bbc113
Bryan Clark <bryan@bclark.net>
2022-03-24 21:14:13 -0400

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeatedString = string;
+  for (let i = 1; i < num; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
